92131470a6fa3fda3b6aa833d3a2d20ea5565e7b
kdybkowski <kdybkowski1@gmail.com>
2023-03-12 19:05:50 +0100

    Add entity.

---
+++ src/UrlShortener.Domain/Entities/Url.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Domain.Entities;
+
+public class Url
+{
+    public int Id { get; set; }
+    public string OriginalUrl { get; set; }
+    public string ShortenedUrl { get; set; }
+} 
