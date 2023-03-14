52171a0c29e326bd305bc422c4e70e4e8796907c
kdybkowski <kdybkowski1@gmail.com>
2023-03-14 17:55:50 +0100

    Add database configuration in code first approach.

---
+++ src/UrlShortener.Data/UrlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Data;
+
+public sealed class UrlDbContext : DbContext
+{
+    public DbSet<Url> Urls { get; set; }
+
+    public UrlDbContext(DbContextOptions<UrlDbContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
