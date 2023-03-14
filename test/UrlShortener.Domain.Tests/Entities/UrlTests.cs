ee8a2f214a18d2d32aa7e2c46fb71aee5b8b19a3
kdybkowski <kdybkowski1@gmail.com>
2023-03-14 21:45:19 +0100

    Add entity unit test.

---
+++ test/UrlShortener.Domain.Tests/Entities/UrlTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Shouldly;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.ValueObjects;
+using Xunit;
+
+namespace UrlShortener.Domain.Tests.Entities;
+
+public class UrlTests
+{
+    private readonly UrlId _urlId = UrlId.Create();
+    private readonly OriginalUrl _originalUrl = new("https://www.example.com");
+    private readonly ShortUrl _shortUrl = new("abc123");
+
+    [Fact]
+    public void changeurlproperties_should_change_properties_correctly()
+    {
+        // Arrange
+        var url = new Url(_urlId, _originalUrl, _shortUrl);
+        var newOriginalUrl = new OriginalUrl("https://www.example.com/new");
+        var newShortUrl = new ShortUrl("def456");
+
+        // Act
+        url.ChangeUrlProperties(newOriginalUrl, newShortUrl);
+
+        // Assert
+        url.OriginalUrl.ShouldBe(newOriginalUrl);
+        url.ShortUrl.ShouldBe(newShortUrl);
+    }
+
+    [Fact]
+    public void changeurlproperties_should_throw_argumentnullexception_when_originalurl_is_null()
+    {
+        // Arrange
+        var url = new Url(_urlId, _originalUrl, _shortUrl);
+
+        // Act & Assert
+        Should.Throw<ArgumentNullException>(() => url.ChangeUrlProperties(null, new ShortUrl("def456")));
+    }
+
+    [Fact]
+    public void changeurlproperties_should_throw_argumentnullexception_when_shorturl_is_null()
+    {
+        // Arrange
+        var url = new Url(_urlId, _originalUrl, _shortUrl);
+
+        // Act & Assert
+        Should.Throw<ArgumentNullException>(() =>
+            url.ChangeUrlProperties(new OriginalUrl("https://www.example.com/new"), null));
+    }
+}
