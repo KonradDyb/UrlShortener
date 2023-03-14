ddcc88bcedf752004867959bf23a76b5f19b04d3
kdybkowski <kdybkowski1@gmail.com>
2023-03-14 18:15:31 +0100

    Add UrlService.

---
+++ src/UrlShortener.Domain/Services/IUrlService.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Domain.Services;
+
+public interface IUrlService
+{
+    Task<Url> ShortenUrl(string url, string alias);
+    Task<IEnumerable<Url>> GetAll();
+    Task<Url> GetByShortUrl(string shortUrl);
+}
---
+++ src/UrlShortener.Domain/Services/UrlService.cs
@@ -0,0 +1,49 @@
+﻿using UrlShortener.Domain.Entities;
+using UrlShortener.Domain.Repositories;
+using UrlShortener.Domain.ValueObjects;
+
+namespace UrlShortener.Domain.Services;
+
+public class UrlService : IUrlService
+{
+    private readonly IUrlRepository _urlRepository;
+
+    public UrlService(IUrlRepository urlRepository)
+    {
+        _urlRepository = urlRepository;
+    }
+
+    public async Task<Url> ShortenUrl(string url, string alias)
+    {
+        var existingUrl = await _urlRepository.GetByOriginalUrl(url);
+        
+        if (existingUrl != null)
+        {
+            return existingUrl;
+        }
+        
+        var newUrl = string.IsNullOrWhiteSpace(alias) ? new Url(UrlId.Create(), new OriginalUrl(url), GenerateShortUrl()) : new Url(UrlId.Create(), new OriginalUrl(url), new ShortUrl(alias));
+
+        await _urlRepository.AddAsync(newUrl);
+        return newUrl;
+    }
+
+    public async Task<IEnumerable<Url>> GetAll()
+    {
+        return await _urlRepository.GetAll();
+    } 
+    public async Task<Url> GetByShortUrl(string shortUrl)
+    {
+        return await _urlRepository.GetByShortUrl(shortUrl);
+    }
+
+    private string GenerateShortUrl()
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        var result = new string(Enumerable.Repeat(chars, 8)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+
+        return result;
+    }
+}
